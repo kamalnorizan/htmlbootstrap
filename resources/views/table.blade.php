ed07f8cec099d7f3508df1cea777668bf7519ed5
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2024-06-21 12:09:49 +0800

    jquery delete

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -21,6 +21,7 @@
                         <table class="table table-success table-striped table-hover table-bordered">
                             <thead>
                                 <tr>
+                                    <th>No</th>
                                     <th>Test Header</th>
                                     <th>Test Header</th>
                                     <th>Test Header</th>
@@ -30,32 +31,68 @@
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td class='no'>1</td>
+                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui
+                                        ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum
+                                        eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui
+                                        ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum
+                                        eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui
+                                        ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum
+                                        eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui
+                                        ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum
+                                        eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td><button class="btn btn-danger deleteBtn">Delete</button></td>
                                 </tr>
                                 <tr>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td class='no'>2</td>
+                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui
+                                        ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum
+                                        eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui
+                                        ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum
+                                        eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui
+                                        ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum
+                                        eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui
+                                        ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum
+                                        eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td><button class="btn btn-danger deleteBtn">Delete</button></td>
                                 </tr>
                                 <tr>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td class='no'>3</td>
+                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui
+                                        ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum
+                                        eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui
+                                        ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum
+                                        eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui
+                                        ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum
+                                        eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui
+                                        ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum
+                                        eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td><button class="btn btn-danger deleteBtn">Delete</button></td>
                                 </tr>
                                 <tr>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td class='no'>4</td>
+                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui
+                                        ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum
+                                        eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui
+                                        ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum
+                                        eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui
+                                        ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum
+                                        eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo placeat est qui
+                                        ducimus molestias repellendus nesciunt assumenda corrupti. Sit mollitia dolorum
+                                        eaque ipsum inventore fugiat saepe eveniet dolores quaerat harum.</td>
+                                    <td><button class="btn btn-danger deleteBtn">Delete</button></td>
                                 </tr>
                             </tbody>
                         </table>
@@ -67,5 +104,39 @@
 @endsection
 
 @section('script')
-    <script></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script>
+        $(document).on("click", ".deleteBtn", function(e) {
+
+            var elem = $(this);
+            swal({
+                title: "Are you sure?",
+                text: "You will not be able to recover this demand point again!",
+                icon: "warning",
+                buttons: {cancel: {
+                    text: "Cancel",
+                    value: null,
+                    visible: true,
+                    className: "",
+                    closeModal: true,
+                },
+                confirm: {
+                    text: "Yes, i'm sure!",
+                    value: true,
+                    visible: true,
+                    className: "btn-danger",
+                    closeModal: true
+                }}
+            }).then((value)=>{
+                if(value==true){
+                    elem.closest("tr").remove();
+                    $.each($('.no'), function (indexInArray, valueOfElement) {
+                        $(valueOfElement).text(indexInArray + 1);
+                    });
+                }
+            });
+        });
+    </script>
 @endsection
